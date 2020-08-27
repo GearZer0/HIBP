87db5419db68865c049f471e0abeb17e0aa15385
GearZer0 <61912722+GearZer0@users.noreply.github.com>
2020-08-27 23:39:20 +0800

    Testing API key response

---
+++ mail_breach.py
@@ -0,0 +1,19 @@
+import requests
+from urllib.parse import quote
+
+
+api_key = '' #place api key
+user_agent = '' #user agent to be placed
+account_list = open('accounts.txt', mode='r', encoding='utf-8').read().split('\n')
+
+headers = {
+    'hibp-api-key' : api_key,
+    'user-agent': user_agent
+}
+
+if __name__ == "__main__":
+    for account in account_list:
+        if account == "":
+            continue
+        resp = requests.get('https://haveibeenpwned.com/api/v3/breachedaccount/{}'.format(account)).text
+        print(resp)
